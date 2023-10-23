4d090dee9f575fd68997ce0f9e4bb7e1ebe84295
jordanferreirasaran <jfsaran@gmail.com>
2023-10-22 23:27:09 -0300

    page root

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
 "use customers"
 
+import SalesTable from "@/components/SalesTable/SalesTable"
+import { SaleProvider } from "@/context/SalesContext"
+
 export default function Home() {
   return (
     <>
+      <SaleProvider>
+        <SalesTable />
+      </SaleProvider>
     </>
   )
 }
